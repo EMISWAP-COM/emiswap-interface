071415d3708c7ee739399fa51452f9e134f93bb9
Ellina Kolisnichenko <ellina.kolisnichenko@firstbridge.io>
2020-10-23 13:20:36 +0300

    Update adresses

---
+++ src/constants/abis/emiswap-vault.ts
@@ -0,0 +1,5 @@
+import EMISWAP_VAULT_ABI from './emiswap-vault.json';
+
+const EMISWAP_VAULT_ADDRESS = process.env.REACT_APP_EMISWAP_VAULT_ADDRESS || '';
+
+export { EMISWAP_VAULT_ADDRESS, EMISWAP_VAULT_ABI };
