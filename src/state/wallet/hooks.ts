e8404f2a5b3ad8e644a1626532f5d016a25ad912
Sergey Sidoruk <sergey.sidoruk@emirex.com>
2020-12-11 13:53:26 +0300

    add logs

--- src/state/wallet/hooks.ts
+++ src/state/wallet/hooks.ts
@@ -196,7 +196,7 @@ export function useCurrencyBalances(
     [currencies],
   );
   const ethBalance = useETHBalances(containsETH ? [account] : []);
-
+  console.log('.....window[\'env\'].REACT_APP_ESW_ID', window['env'].REACT_APP_ESW_ID)
   const containsESW: boolean = useMemo(
     () => currencies?.some(currency => currency?.address === window['env'].REACT_APP_ESW_ID) ?? false,
     [currencies],
