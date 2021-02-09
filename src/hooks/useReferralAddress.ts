9f312533093e7c215289f8ef5cba98e68502d2c4
alexandrvorotov <76166408+alexandrvorotov@users.noreply.github.com>
2021-02-09 13:57:08 +0300

    Development (#127)
    * calculation of the package purchase price on the basis of the previous package (#121)
    
    * hot fix
    
    * get referral address from BE (#124)
    
    * refactor useReferralAddress (#126)

--- src/hooks/useReferralAddress.ts
+++ src/hooks/useReferralAddress.ts
@@ -1,9 +1,10 @@
-import { REFERRAL_ADDRESS_STORAGE_KEY } from '../constants';
 import { useActiveWeb3React } from './index';
 import { ZERO_ADDRESS } from '../constants/one-split';
+import { AppState } from '../state';
+import { useSelector } from 'react-redux';
 
 export const useReferralAddress = () => {
-  const addressStorage = localStorage.getItem(REFERRAL_ADDRESS_STORAGE_KEY);
+  const walletAddress = useSelector((state: AppState) => state.user.info.walletAddress);
   const { account } = useActiveWeb3React();
-  return !addressStorage || account === addressStorage ? ZERO_ADDRESS : addressStorage;
+  return !walletAddress || account === walletAddress ? ZERO_ADDRESS : walletAddress;
 };
