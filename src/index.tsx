93f8cff45556b7d63d469546b1c719c83ba0084d
Nikita Bezhin <bezhin.nikita@gmail.com>
2021-03-31 13:45:34 +0300

    ES-475 Polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -59,6 +59,14 @@ window.addEventListener('error', error => {
   })
 })
 
+Object.defineProperty(Array.prototype, 'flat', {
+  value: function(depth = 1) {
+    return this.reduce(function (flat, toFlatten) {
+      return flat.concat((Array.isArray(toFlatten) && (depth>1)) ? toFlatten.flat(depth-1) : toFlatten);
+    }, []);
+  }
+});
+
 function Updaters() {
   return (
     <>
