9c805ba20cb814cb97f2a10db8c2a28f3cb2b9be
Mikhail Pushkov <pushkov@evrone.com>
2021-04-15 15:07:43 +0300

    revert deposit ref history from referral api

--- src/components/AccountDetails/Common/PurchaseHistory.tsx
+++ src/components/AccountDetails/Common/PurchaseHistory.tsx
@@ -4,6 +4,7 @@ import { Header } from '../styleds';
 import { useSelector } from 'react-redux';
 import { AppState } from '../../../state';
 import { convertBigDecimal, convertDate, DateFormat, shortenHash } from '../uitls';
+import { Level } from '../styleds';
 
 const Table = styled.div<{ amount?: number }>`
   color: ${({ theme }) => theme.grey6};
@@ -104,18 +105,10 @@ const NoContent = styled.div`
 `;
 
 export const PurchaseHistory = () => {
-  // const purchases = useSelector((state: AppState) => state.cabinets.purchaseHistory);
-  // const { referrals } = useSelector((state: AppState) => state.cabinets.performance);
+  const { referrals } = useSelector((state: AppState) => state.cabinets.performance);
   const { histories } = useSelector((state: AppState) => state.cabinets.balance);
-  // console.log('total');
   const deposit = histories?.deposits;
-  const referrals = [
-    {
-      deposits: histories?.referral_bonus || [],
-    },
-  ];
 
-  console.log(useSelector((state: AppState) => state.cabinets.performance));
   return (
     <>
       <Header>Your Purchase History</Header>
@@ -142,12 +135,12 @@ export const PurchaseHistory = () => {
       <Header>Referral Purchase History</Header>
       <TableLong amount={referrals.length}>
         {referrals &&
-          referrals.map(({ deposits }) => {
+          referrals.map(({ deposits, level }) => {
             return deposits.map(({ transaction_hash, amount, token, created_at }) => (
               <TableRow key={transaction_hash}>
                 <Date>{convertDate(created_at, DateFormat.full)}</Date>
                 <LevelWrapper>
-                  {/*<Level>{level}lvl</Level>*/}
+                  <Level>{level}lvl</Level>
                   <Cost>
                     <span>{convertBigDecimal(amount)}</span>&nbsp; {token}
                   </Cost>
@@ -157,7 +150,7 @@ export const PurchaseHistory = () => {
               </TableRow>
             ));
           })}
-        {!referrals[0].deposits.length && (
+        {!referrals.length && (
           <TableRow>
             <NoContent>No content</NoContent>
           </TableRow>
