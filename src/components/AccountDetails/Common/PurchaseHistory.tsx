35480cb6d9de71e6c11dd5ac5900e37c5417e7c0
Mikhail Pushkov <pushkov@evrone.com>
2021-04-21 18:59:59 +0300

    add Compensation table styles

--- src/components/AccountDetails/Common/PurchaseHistory.tsx
+++ src/components/AccountDetails/Common/PurchaseHistory.tsx
@@ -13,8 +13,9 @@ const Table = styled.div<{ amount?: number }>`
   overflow-y: auto;
 
   @media screen and (max-width: 1200px) {
-    max-height: 210px;
+    max-height: 310px;
     background: none;
+    margin-top: 20px;
   }
 
   //&::-webkit-scrollbar {
@@ -26,50 +27,52 @@ const TableLong = styled(Table)`
   max-height: 200px;
 `;
 
+const TableFeeCompensation = styled(Table)`
+  @media screen and (max-width: 1200px) {
+    max-height: 414px;
+  }
+`;
+
 const TableRow = styled.div`
-  height: 40px;
+  height: 46px;
   display: flex;
   border-radius: 3px;
-  align-items: center;
   font-size: 0.8rem;
+  align-items: center;
   padding: 0 1rem;
-
-  &:nth-child(2n - 1) {
-    background: ${({ theme }) => theme.bg2};
-  }
+  border-bottom: ${({ theme }) => `1px solid ${theme.grey1}`};
 
   @media screen and (max-width: 1200px) {
-    height: 70px;
-    flex-wrap: wrap;
-    justify-content: space-between;
+    flex-direction: column;
+    height: auto;
+    padding: 0;
   }
 `;
 
 const DateField = styled.div`
-  min-width: 8.5rem;
+  min-width: 11rem;
+  @media screen and (max-width: 1200px) {
+    position: relative;
+    left: -1rem;
+    font-weight: 600;
+    color: ${({ theme }) => theme.text1};
+  }
 `;
 
 const LevelWrapper = styled.div`
   display: flex;
-  align-items: center;
-  min-width: 1.5rem;
-  align-self: center;
+  min-width: 8rem;
 
   @media screen and (max-width: 1200px) {
-    min-width: 190px;
     justify-content: flex-end;
   }
 `;
 
 const Cost = styled.div`
   min-width: 8rem;
-  text-align: right;
-  margin-right: 1rem;
 
   @media screen and (max-width: 1200px) {
-    margin-right: 0;
     min-width: auto;
-    margin-left: 5px;
   }
 
   > span {
@@ -80,7 +83,6 @@ const Cost = styled.div`
 
 const Wallet = styled.div`
   color: ${({ theme }) => theme.text1};
-  margin-left: auto;
 
   @media screen and (max-width: 1200px) {
     font-weight: 500;
@@ -95,6 +97,39 @@ const NoContent = styled.div`
   @media screen and (max-width: 1200px) {
     font-weight: 500;
     font-size: 0.75rem;
+  }
+`;
+
+const Cell = styled.div`
+  @media screen and (max-width: 1200px) {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: space-between;
+    height: 34px;
+    padding: 0 1rem;
+
+    &:nth-child(2n) {
+      background: ${({ theme }) => theme.bg2};
+    }
+  }
+`;
+
+const TableTitles = styled(TableRow)`
+  background: ${({ theme }) => theme.bg2};
+  height: 30px;
+  border-bottom: none;
+
+  @media screen and (max-width: 1200px) {
+    display: none;
+  }
+`;
+
+const Label = styled.span`
+  display: none;
+
+  @media screen and (max-width: 1200px) {
+    display: flex;
   }
 `;
 
@@ -135,63 +170,112 @@ export const PurchaseHistory = () => {
 
   return (
     <>
-      <Header>Your Purchase History</Header>
-      <Table amount={deposit.length}>
-        {deposit &&
-          deposit.map(({ amount, token, created_at, transaction_hash }) => (
-            <TableRow key={transaction_hash + created_at}>
+      {/*<Header>Your Purchase History</Header>*/}
+      {/*<TableTitles>*/}
+      {/*    <DateField>Timestamp</DateField>*/}
+      {/*    <Cost>Purchased tokens</Cost>*/}
+      {/*    <Wallet>Txhash</Wallet>*/}
+      {/*</TableTitles>*/}
+      {/*<Table amount={deposit.length}>*/}
+      {/*  {deposit &&*/}
+      {/*    deposit.map(({ amount, token, created_at, transaction_hash }, index) => (*/}
+      {/*      <TableRow key={transaction_hash + created_at}>*/}
+      {/*        <Cell>*/}
+      {/*          <DateField>{convertDate(created_at, DateFormat.full)}</DateField>*/}
+      {/*        </Cell>*/}
+      {/*        <Cell>*/}
+      {/*          <Label>Purchased tokens</Label>*/}
+      {/*          <LevelWrapper>*/}
+      {/*            <Cost>*/}
+      {/*              <span>{convertBigDecimal(amount)}</span>&nbsp; {token}*/}
+      {/*            </Cost>*/}
+      {/*          </LevelWrapper>*/}
+      {/*        </Cell>*/}
+      {/*        <Cell>*/}
+      {/*          <Label>Txhash</Label>*/}
+      {/*          <Wallet>{shortenHash(transaction_hash, 7)}</Wallet>*/}
+      {/*        </Cell>*/}
+      {/*      </TableRow>*/}
+      {/*    ))}*/}
+      {/*  {!deposit.length && (*/}
+      {/*    <TableRow>*/}
+      {/*      <NoContent>No content</NoContent>*/}
+      {/*    </TableRow>*/}
+      {/*  )}*/}
+      {/*</Table>*/}
+
+      {/*<Header>Referral Purchase History</Header>*/}
+      {/*<TableTitles>*/}
+      {/*  <DateField>Timestamp</DateField>*/}
+      {/*  <Cost>Purchased tokens</Cost>*/}
+      {/*  <Wallet>Txhash</Wallet>*/}
+      {/*</TableTitles>*/}
+      {/*<TableLong amount={referrals.length}>*/}
+      {/*  {referrals &&*/}
+      {/*    referrals.map(({ deposits, level }) => {*/}
+      {/*      return deposits.map(({ transaction_hash, amount, token, created_at }) => (*/}
+      {/*        <TableRow key={transaction_hash + created_at}>*/}
+      {/*          <Cell>*/}
+      {/*            <DateField>{convertDate(created_at, DateFormat.full)}</DateField>*/}
+      {/*          </Cell>*/}
+      {/*          <Cell>*/}
+      {/*            <Label>Purchased tokens</Label>*/}
+      {/*            <LevelWrapper>*/}
+      {/*              <Level>{level}lvl</Level>*/}
+      {/*              <Cost>*/}
+      {/*                <span>{convertBigDecimal(amount)}</span>&nbsp; {token}*/}
+      {/*              </Cost>*/}
+      {/*            </LevelWrapper>*/}
+      {/*          </Cell>*/}
+      {/*          <Cell>*/}
+      {/*            <Label>Txhash</Label>*/}
+      {/*            <Wallet>{shortenHash(transaction_hash, 7)}</Wallet>*/}
+      {/*          </Cell>*/}
+      {/*        </TableRow>*/}
+      {/*      ));*/}
+      {/*    })}*/}
+      {/*  {!referrals.length && (*/}
+      {/*    <TableRow>*/}
+      {/*      <NoContent>No content</NoContent>*/}
+      {/*    </TableRow>*/}
+      {/*  )}*/}
+      {/*</TableLong>*/}
+
+      <Header>Your Fee Compensation History</Header>
+      <TableTitles>
+        <DateField>Timestamp</DateField>
+        <Cost>Transaction fee</Cost>
+        <Cost>Received tokens</Cost>
+        <Wallet>Txhash</Wallet>
+      </TableTitles>
+      <TableFeeCompensation amount={deposit.length}>
+        {compensation.map(({ amount, token, created_at, transaction_hash }) => (
+          <TableRow key={transaction_hash + created_at}>
+            <Cell>
               <DateField>{convertDate(created_at, DateFormat.full)}</DateField>
+            </Cell>
+            <Cell>
+              <Label>Transaction fee</Label>
+
+              <LevelWrapper>
+                <Cost>
+                  <span>-</span>&nbsp; USD
+                </Cost>
+              </LevelWrapper>
+            </Cell>
+            <Cell>
+              <Label>Received tokens</Label>
+
               <LevelWrapper>
                 <Cost>
                   <span>{convertBigDecimal(amount)}</span>&nbsp; {token}
                 </Cost>
               </LevelWrapper>
+            </Cell>
+            <Cell>
+              <Label>Txhash</Label>
               <Wallet>{shortenHash(transaction_hash, 7)}</Wallet>
-            </TableRow>
-          ))}
-        {!deposit.length && (
-          <TableRow>
-            <NoContent>No content</NoContent>
-          </TableRow>
-        )}
-      </Table>
-
-      <Header>Referral Purchase History</Header>
-      <TableLong amount={referrals.length}>
-        {referrals &&
-          referrals.map(({ deposits, level }) => {
-            return deposits.map(({ transaction_hash, amount, token, created_at }) => (
-              <TableRow key={transaction_hash + created_at}>
-                <DateField>{convertDate(created_at, DateFormat.full)}</DateField>
-                <LevelWrapper>
-                  <Level>{level}lvl</Level>
-                  <Cost>
-                    <span>{convertBigDecimal(amount)}</span>&nbsp; {token}
-                  </Cost>
-                </LevelWrapper>
-
-                <Wallet>{shortenHash(transaction_hash, 7)}</Wallet>
-              </TableRow>
-            ));
-          })}
-        {!referrals.length && (
-          <TableRow>
-            <NoContent>No content</NoContent>
-          </TableRow>
-        )}
-      </TableLong>
-
-      <Header>Your Fee Compensation History</Header>
-      <Table amount={deposit.length}>
-        {compensation.map(({ amount, token, created_at, transaction_hash }) => (
-          <TableRow key={transaction_hash + created_at}>
-            <DateField>{convertDate(created_at, DateFormat.full)}</DateField>
-            <LevelWrapper>
-              <Cost>
-                <span>{convertBigDecimal(amount)}</span>&nbsp; {token}
-              </Cost>
-            </LevelWrapper>
-            <Wallet>{shortenHash(transaction_hash, 7)}</Wallet>
+            </Cell>
           </TableRow>
         ))}
         {!deposit.length && (
@@ -199,7 +283,7 @@ export const PurchaseHistory = () => {
             <NoContent>No content</NoContent>
           </TableRow>
         )}
-      </Table>
+      </TableFeeCompensation>
 
       <Header>Your Swapping History</Header>
       <Table amount={deposit.length}>
