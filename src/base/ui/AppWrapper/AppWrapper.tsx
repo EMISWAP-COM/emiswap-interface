e8dc91d0115155be8a162ef9ed0558e2866a87df
Maxim Baranov <maximb@evrone.com>
2021-05-25 15:33:15 +0300

    Revert "Move bg to separate layer"
    This reverts commit 25735c91

--- src/base/ui/AppWrapper/AppWrapper.tsx
+++ src/base/ui/AppWrapper/AppWrapper.tsx
@@ -1,37 +1,15 @@
-import React from 'react';
 import styled from 'styled-components'
 import backgroundCurvedLines from '../../../assets/images/backgroundCurvedLines.svg'
 
-export const AppWrapper: React.FC = (
-  {
-    children,
-  }
-) => {
-  const BackgroundContainer = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  max-height: 1390px;
-  background-image: url('${backgroundCurvedLines}');
-  background-position: top;
-  background-repeat: no-repeat;
-  background-size: 1920px 1390px;
-  `;
-
-  const Wrapper = styled.div`
-  position: relative;
+export const AppWrapper = styled.div`
   display: flex;
   flex-flow: column;
   align-items: flex-start;
   overflow-x: hidden;
   // height: 100vh;
+  background-image: url('${backgroundCurvedLines}');
   background-color: #000;
+  background-position: top;
+  background-repeat: no-repeat;
+  background-size: 1920px 1390px;
 `;
-
-  return (<Wrapper>
-    <BackgroundContainer />
-    {children}
-  </Wrapper>);
-}
