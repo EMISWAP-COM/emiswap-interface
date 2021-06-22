24b3bc69a6879e950743de836e9e87edd0d4c366
Alexey Karasev <alex.deimos@emidao.org>
2021-06-22 21:18:35 +0700

    ES-1088 - Error after connecting Formatic wallet  - Finding the bug

--- src/hooks/useTokenListWithPair.ts
+++ src/hooks/useTokenListWithPair.ts
@@ -4,8 +4,7 @@ import { useEffect, useState } from 'react';
 import { getContract } from '../utils';
 import { useActiveWeb3React } from './index';
 import { addPopup } from '../state/application/actions';
-import { useDispatch } from 'react-redux'
-
+import { useDispatch } from 'react-redux';
 
 export function useTokenListWithPair() {
   const { library, account } = useActiveWeb3React();
@@ -14,25 +13,47 @@ export function useTokenListWithPair() {
 
   const factoryContract = useEmiFactoryContract();
   const emiSwapContract = useEmiSwapContract();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+
+  // todo: Testing for task https://emi.myjetbrains.com/youtrack/issue/ES-1088
+  const DEBUG = true;
 
   useEffect(() => {
     if (factoryContract && emiSwapContract) {
       setIsLoading(true);
-      factoryContract.getAllPools().then((data: string[]) => {
-        const promiseMap = data.map(el => {
+      DEBUG && console.log('Before getAllPools()', { factoryContract });
+      factoryContract
+        .getAllPools()
+        .then((data: string[]) => {
+          const promiseMap = data.map(el => {
             const contract = getContract(el, EMI_SWAP_ABI, library!, account ? account : undefined);
-            return contract.getTokens();
-        });
-        Promise.all(promiseMap).then((data: string[][]) => {
-          setIsLoading(false);
-          const formattedData = data
-            .flat()
-            .filter((ads, idx, arr) => arr.findIndex(el => el === ads) === idx);
-          setList(formattedData);
-        });
-      })
-        .catch(e => {
+
+            const result = contract.getTokens();
+            DEBUG &&
+              console.log('Then promiseMap', {
+                data,
+                contract,
+                el,
+                EMI_SWAP_ABI,
+                library,
+                account,
+                result,
+              });
+
+            return result;
+          });
+          Promise.all(promiseMap).then((data: string[][]) => {
+            DEBUG && console.log('Then Promise.all', { data });
+
+            setIsLoading(false);
+            const formattedData = data
+              .flat()
+              .filter((ads, idx, arr) => arr.findIndex(el => el === ads) === idx);
+            setList(formattedData);
+          });
+        })
+        .catch((e: Error) => {
+          DEBUG && console.log('Catch', { e, EMI_SWAP_ABI, library, account });
           dispatch(
             addPopup({
               key: 'useEmiFactoryContract',
@@ -46,6 +67,6 @@ export function useTokenListWithPair() {
           );
         });
     }
-  }, [factoryContract, emiSwapContract, account, library, dispatch]);
+  }, [factoryContract, emiSwapContract, account, library, dispatch, DEBUG]);
   return [list, isLoading] as [string[], boolean];
 }
