38d1856fd42dcc7a58799fe7b33e4db3c1f549ea
Maxim Baranov <maximb@evrone.com>
2021-07-01 15:55:03 +0300

    Added fallback when wallet is not connected

--- src/pages/Farm/index.tsx
+++ src/pages/Farm/index.tsx
@@ -9,6 +9,8 @@ import { Contract } from '@ethersproject/contracts';
 import { getFarmingContracts } from '../../utils';
 import { useActiveWeb3React } from '../../hooks';
 import FarmComponent from './FarmComponent';
+import Button from '../../base/ui/Button';
+import { useWalletModalToggle } from '../../state/application/hooks';
 
 const StyledFarmingHeader = styled.div`
   display: flex;
@@ -81,23 +83,36 @@ export default function Farm() {
 
   const farmingContracts: Contract[] = useMemo(() => getFarmingContracts(library, account), [library, account]);
 
+  const toggleWalletModal = useWalletModalToggle();
+
   return (
     <>
       <AppBody>
         <SwapPoolTabs active={TabNames.FARM} />
-        <StyledFarmingHeader>
-          <StyledTabs><Tabs items={tabItems} selectedItemId={selectedTab} onChange={setSelectedTab} /></StyledTabs>
-          <RadioGroup buttonsList={radioList} groupName="farms" value={radioValue} onChange={setRadioValue} />
-        </StyledFarmingHeader>
-        <StyledInfoWrapper>
-          <StyledInfoTitle>Variable APR Farms</StyledInfoTitle>
-          <StyledInfo>
-            Use these farms to increase your profit from different tokens, including ESW.
-            No time limits let you stake tokens as long as you wish.
-            Farming rewards are allocated to your EmiSwap account for every block.
-          </StyledInfo>
-        </StyledInfoWrapper>
-        {farmingContracts.map((contract) => <FarmComponent key={contract.address} contract={contract} />)}
+        {account && (
+          <>
+            <StyledFarmingHeader>
+              <StyledTabs><Tabs items={tabItems} selectedItemId={selectedTab} onChange={setSelectedTab} /></StyledTabs>
+              <RadioGroup buttonsList={radioList} groupName="farms" value={radioValue} onChange={setRadioValue} />
+            </StyledFarmingHeader>
+            <StyledInfoWrapper>
+              <StyledInfoTitle>Variable APR Farms</StyledInfoTitle>
+              <StyledInfo>
+                Use these farms to increase your profit from different tokens, including ESW.
+                No time limits let you stake tokens as long as you wish.
+                Farming rewards are allocated to your EmiSwap account for every block.
+              </StyledInfo>
+            </StyledInfoWrapper>
+            {farmingContracts.map((contract) => <FarmComponent key={contract.address} contract={contract} />)}
+          </>
+        )}
+        {!account && (
+          <>
+            Please connect your wallet to see all available farms and staking pools
+            <br/><br/>
+            <Button onClick={toggleWalletModal}>Connect to a wallet</Button>
+          </>
+        )}
       </AppBody>
     </>
   );
