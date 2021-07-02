a941bae3a9f09b45603336c8a6e27ca481e76541
Вячеслав Дутка <vyacheslav.dutka@emirex.com>
2021-07-02 22:54:45 +0300

    Restore tokens for swap and add 3 LP Farms

--- src/pages/Farm/isLpToken.ts
+++ src/pages/Farm/isLpToken.ts
@@ -1,5 +1,6 @@
 import { Token } from '@uniswap/sdk';
 
+//FIXME Replace with check by Token mode
 const isLpToken = (token: Token) => token && token.name.indexOf('-') !== -1;
 
 export default isLpToken;
