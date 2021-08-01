d74e656dfb015570fe7b916d1eb7135938c6175a
Вячеслав Дутка <vyacheslav.dutka@emirex.com>
2021-08-01 14:26:00 +0300

    Change stablecoin order

--- src/pages/Farm/getEswPriceInDai.ts
+++ src/pages/Farm/getEswPriceInDai.ts
@@ -6,20 +6,33 @@ import { tokenAmountToString } from '../../utils/formats';
 
 const getEswPriceInDai = (library: Web3Provider, account: string, chainId: number) => {
   const emiPrice2 = getEmiPrice2Contract(library, account);
-  const eswCoin = defaultCoins.tokens.find((token) => token.chainId === chainId && token.symbol === 'ESW');
-  const daiCoin = defaultCoins.tokens.find((token) => token.chainId === chainId && token.symbol === 'DAI');
-  return emiPrice2.getCoinPrices(
-    [eswCoin.address],
-    [daiCoin.address],
-    0
-  ).then((value: BigInt[]) => {
-    const daiToken = new Token(chainId, daiCoin.address, daiCoin.decimals, daiCoin.symbol, daiCoin.name);
-    const tokenAmount = new TokenAmount(
-      daiToken,
-      JSBI.BigInt(value.toString())
+  const eswCoin = defaultCoins.tokens.find(
+    token => token.chainId === chainId && token.symbol === 'ESW',
+  );
+  const daiCoin = defaultCoins.tokens.find(
+    token => token.chainId === chainId && token.symbol === 'DAI',
+  );
+  const usdtCoin = defaultCoins.tokens.find(
+    token => token.chainId === chainId && token.symbol === 'USDT',
+  );
+  const usdсCoin = defaultCoins.tokens.find(
+    token => token.chainId === chainId && token.symbol === 'USDС',
+  );
+  const stableTokens = [usdtCoin?.address, daiCoin?.address, usdсCoin?.address].filter(
+    value => value !== undefined,
+  );
+
+  return emiPrice2.getCoinPrices([eswCoin.address], stableTokens, 0).then((value: BigInt[]) => {
+    const daiToken = new Token(
+      chainId,
+      daiCoin.address,
+      daiCoin.decimals,
+      daiCoin.symbol,
+      daiCoin.name,
     );
+    const tokenAmount = new TokenAmount(daiToken, JSBI.BigInt(value.toString()));
     return tokenAmountToString(tokenAmount, daiToken.decimals);
   });
-}
+};
 
 export default getEswPriceInDai;
