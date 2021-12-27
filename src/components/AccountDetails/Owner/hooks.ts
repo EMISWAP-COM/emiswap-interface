85ad984195a973f057b153bb4709f1fc2229e1bc
Alexey Semikozov <alwexy@gmail.com>
2021-12-27 15:59:22 +0300

    Add request collect with oracle

--- src/components/AccountDetails/Owner/hooks.ts
+++ src/components/AccountDetails/Owner/hooks.ts
@@ -4,14 +4,12 @@ import { AppState } from '../../../state';
 import { getCollectContract } from '../../../utils';
 import { Contract } from '@ethersproject/contracts';
 import { parseUnits } from '@ethersproject/units';
-// import { useAuth } from '../../../hooks/useAuth';
+import { useAuth } from '../../../hooks/useAuth';
 import { useSelector } from 'react-redux';
-// import { fetchWrapper } from '../../../api/fetchWrapper';
-// import { EMI_DELIVERY } from '../../../constants/emi/addresses';
+import { fetchWrapper } from '../../../api/fetchWrapper';
+import { EMI_DELIVERY } from '../../../constants/emi/addresses';
 
-// const ESW_CLAIM_API = window['env'].REACT_APP_ESW_CLAIM_API;
-const randomSignature =
-  'eed2d9de70b25f4de060d41ad1aa894bb78920d6668d67695b592a89d3261275108ded542513ee4073119d9d809b363d00ecded1801e71927c6ba93b28e2d58e1b';
+const ESW_CLAIM_API = window['env'].REACT_APP_ESW_CLAIM_API;
 
 export const useRequestCollect = (userInput: string, closeWindow: () => void) => {
   const { library, account, chainId } = useActiveWeb3React();
@@ -20,6 +18,8 @@ export const useRequestCollect = (userInput: string, closeWindow: () => void) =>
     available: { ESW: availableESW },
   } = useSelector((state: AppState) => state.cabinets.totalBalance);
   const contract: Contract | null = getCollectContract(library, account, chainId);
+  const handleAuth = useAuth();
+  const { id: userID } = useSelector((state: AppState) => state.user.info);
 
   try {
     parseUnits(userInput, 18).toString();
@@ -30,53 +30,47 @@ export const useRequestCollect = (userInput: string, closeWindow: () => void) =>
     return { isValidInput };
   }
   const amount = parseUnits(userInput, 18).toString();
-  // const handleAuth = useAuth();
-  // const [availableContact, setAvailableContact] = useState<string>('null');
-  // const { id: userID } = useSelector((state: AppState) => state.user.info);
 
   const handler = () => {
     contract
       .getWalletNonce()
-      .then(nonce => nonce + 1)
-      .then(nonce => {
-        return contract.requestUnsigned(account, amount, nonce, `0x${randomSignature}`).then(() => {
-          closeWindow();
-        });
-
-        // start TODO revert then sign server work correct
-        // return handleAuth().then(token => {
-        //   fetchWrapper
-        //     .post(ESW_CLAIM_API, {
-        //       body: JSON.stringify({
-        //         token_name: 'ESW',
-        //         amount,
-        //         contract_address: EMI_DELIVERY,
-        //         nonce,
-        //         // TODO: use from env
-        //         blockchain_network: 'polygon_test',
-        //         chainID: 'ETH_KV',
-        //         userID,
-        //       }),
-        //       headers: { Authorization: token },
-        //     })
-        //     .then(({ signature }) => {
-        //       return contract.request(account, amount, nonce, `0x${signature}`);
-        //     });
-        // });
-        // end TODO revert then sign server work correct
-
-        // start TODO request PUT
-        // .then(transactionResult => {
-        //   const transactionStateEndPoint = `${baseUrl}/v1/private/users/${userID}/transactions/${id}`;
-        //   fetchWrapper.put(transactionStateEndPoint, {
-        //     headers: {
-        //       authorization: authToken,
-        //     },
-        //     body: JSON.stringify(transactionResult),
-        //   });
-        // });
-        // end TODO request PUT
-      });
+      .then(nonce => Number(nonce) + 1)
+      .then(nonce =>
+        handleAuth().then(token => {
+          fetchWrapper
+            .post(ESW_CLAIM_API, {
+              body: JSON.stringify({
+                token_name: 'ESW',
+                amount,
+                contract_address: EMI_DELIVERY,
+                nonce,
+                // TODO: use from env
+                blockchain_network: 'polygon_test',
+                chainID: 'ETH_KV',
+                userID,
+              }),
+              headers: { Authorization: token },
+            })
+            .then(({ signature, id }) => {
+              return contract
+                .request(account, amount, nonce, `0x${signature}`)
+                .then(transactionResult => [transactionResult, id]);
+            })
+            .then(([transactionResult, id]) => {
+              const transactionStateEndPoint = `/v1/private/users/${userID}/transactions/${id}`;
+              fetchWrapper.put(transactionStateEndPoint, {
+                headers: {
+                  authorization: token,
+                },
+                body: JSON.stringify({
+                  state: 'sent',
+                  transaction_hash: transactionResult.hash,
+                }),
+              });
+              closeWindow();
+            });
+        }),
+      );
   };
 
   return { handler, availableReqestCollect: availableESW, isValidInput };
