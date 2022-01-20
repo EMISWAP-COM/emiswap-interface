8f76d2c4a0377cf36cd29e24e936ccca09787a7c
Денис Лукин <denis.lukin@simbirsoft.com>
2022-01-20 14:56:59 +0300

    fixed failed pipeline https://gitlab.com/emidao/emiswap/ui/-/pipelines/452426740/failures

--- src/hooks/Tokens.ts
+++ src/hooks/Tokens.ts
@@ -150,6 +150,7 @@ export function useAllTokens(isLpTokens?: boolean): [{ [address: string]: Token 
       isKuCoinActive,
       isPolygonActive,
       isShidenActive,
+      isAvalancheActive,
     ]),
     isLoading,
   ];
