ee4192e78fe214dee8710fdf2f2b012d8f3763e5
Alexey Semikozov <alwexy@gmail.com>
2022-02-09 15:49:37 +0300

    Add status behavior

--- src/pages/Bridge/Status/Status.tsx
+++ src/pages/Bridge/Status/Status.tsx
@@ -25,57 +25,25 @@ const Status = ({ fromChain, toChain, step }) => {
           <S.Timer>{seconds}</S.Timer>
         </S.Column>
         <S.Column>
-          <span>Cost of Gas</span>
-          <S.Cost>$999</S.Cost>
-        </S.Column>
-      </S.Grid>
-
-      <SC.Process>
-        <BigDot active />
-        <SC.Delimiter active />
-        <BigDot active />
-        <SC.Delimiter />
-        <BigDot />
-        <SC.Delimiter />
-        <BigDot />
-      </SC.Process>
-      <S.Text>
-        <span>Start</span>
-        <span>1/35</span>
-        <span>0/35</span>
-        <span>Final</span>
-      </S.Text>
-
-      <S.HR />
-
-      <S.Grid>
-        <S.Column>
           <span>Token</span>
           <S.Token>
             <S.Icon src={toChain.icon} />
             {toChain.name}
           </S.Token>
         </S.Column>
-        <S.Column />
-        <S.Column>
-          <span>Timer</span>
-          <S.Timer>00:00</S.Timer>
-        </S.Column>
       </S.Grid>
 
       <SC.Process>
-        <BigDot />
-        <SC.Delimiter />
-        <BigDot />
-        <SC.Delimiter />
-        <BigDot />
-        <SC.Delimiter />
-        <BigDot />
+        <BigDot active={step > 0} />
+        <SC.Delimiter active={step > 1} />
+        <BigDot active={step > 1} />
+        <SC.Delimiter active={step > 2} />
+        <BigDot active={step > 2} />
+        <SC.Delimiter active={step > 3} />
+        <BigDot active={step > 3} />
       </SC.Process>
       <S.Text>
         <span>Start</span>
-        <span>1/35</span>
-        <span>0/35</span>
         <span>Final</span>
       </S.Text>
     </SC.Root>
