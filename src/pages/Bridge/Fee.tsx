8325a2e132c10705f6eb6013151c3f19cc0bf43b
Alexey Semikozov <alwexy@gmail.com>
2022-02-16 19:33:49 +0300

    Fix text fees

--- src/pages/Bridge/Fee.tsx
+++ src/pages/Bridge/Fee.tsx
@@ -3,19 +3,29 @@ import useFeesByRoute from './hooks/useFeesByRoute';
 import { useQuoteData } from './hooks/useQuoteData';
 import * as S from './styled';
 
-const Fee = () => {
-  const { isSuccess, quotes } = useQuoteData();
+const getFeeText = (isFetchingQuote: boolean, fee?: string): string => {
+  if (isFetchingQuote) {
+    return 'Loading...';
+  }
+  if (!fee) {
+    return 'Unknown';
+  }
+  return fee;
+};
 
+const Fee = () => {
+  const { isSuccess, isFetching, quotes } = useQuoteData();
   const { fees } = useFeesByRoute(isSuccess ? quotes?.routes?.[0] : null);
+
   return (
     <S.Fee>
       <S.FeeRow>
         <S.FeeRowLabel>Transaction Fee</S.FeeRowLabel>
-        <S.FeeRowValue>{fees.transactionFee || 'Unknown'}</S.FeeRowValue>
+        <S.FeeRowValue>{getFeeText(isFetching, fees.transactionFee)}</S.FeeRowValue>
       </S.FeeRow>
       <S.FeeRow>
         <S.FeeRowLabel>Bridge Fee</S.FeeRowLabel>
-        <S.FeeRowValue>{fees.bridgeFee || 'Unknown'}</S.FeeRowValue>
+        <S.FeeRowValue>{getFeeText(isFetching, fees.bridgeFee)}</S.FeeRowValue>
       </S.FeeRow>
     </S.Fee>
   );
