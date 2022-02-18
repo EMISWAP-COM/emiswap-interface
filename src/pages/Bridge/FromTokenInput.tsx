46235d5611f6f7d4e9285c79ce45c45551cbbd94
Alexey Semikozov <alwexy@gmail.com>
2022-02-18 17:24:16 +0300

    Fix conditional queries

--- src/pages/Bridge/FromTokenInput.tsx
+++ src/pages/Bridge/FromTokenInput.tsx
@@ -1,27 +1,17 @@
 import React, { useCallback, useEffect, useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import { useAppSelector } from 'state/hooks';
-import { useGetSupportTokenFromQuery } from './api';
-import {
-  selectAmountFromToken,
-  selectFromToChains,
-  selectFromToken,
-  setAmountFromToken,
-  setFromToken,
-} from './slice';
+import { useTokenListFrom } from './hooks';
+import { selectAmountFromToken, selectFromToken, setAmountFromToken, setFromToken } from './slice';
 import TokenInput from './TokenInput';
 import { Token } from './types';
 import { getUniqueTokens } from './utils';
 
 const FromTokenInput = () => {
-  const { fromChain, toChain } = useAppSelector(selectFromToChains);
-  const { data, isSuccess } = useGetSupportTokenFromQuery({
-    fromChain,
-    toChain,
-  });
   const fromToken = useAppSelector(selectFromToken);
   const amount = useAppSelector(selectAmountFromToken);
   const dispatch = useDispatch();
+  const { data, isSuccess } = useTokenListFrom();
 
   const setToken = useCallback((value: Token) => dispatch(setFromToken(value)), [
     dispatch,
