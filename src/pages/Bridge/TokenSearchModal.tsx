fbd36f5280868a576f4322ff70fc7aac945b542a
DENIS LUKIN <nash1q@yandex.ru>
2022-02-21 15:50:40 +0300

    Rewrited search windows

--- src/pages/Bridge/TokenSearchModal.tsx
+++ src/pages/Bridge/TokenSearchModal.tsx
@@ -1,55 +1,149 @@
-import React, { KeyboardEvent, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { Token, TokenAmount } from '@uniswap/sdk';
+import Card from 'components/Card';
+import QuestionHelper from 'components/QuestionHelper';
+import { AutoRow, RowBetween } from 'components/Row';
+import { filterTokens } from 'components/SearchModal/filtering';
+import SortButton from 'components/SearchModal/SortButton';
+import { useTokenComparator } from 'components/SearchModal/sorting';
+import { useActiveWeb3React } from 'hooks';
+import { useAllTokens, useToken } from 'hooks/Tokens';
+import { Text } from 'rebass';
+import useInterval from 'hooks/useInterval';
+import React, {
+  KeyboardEvent,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import { isMobile } from 'react-device-detect';
 import { useTranslation } from 'react-i18next';
+import { useAllTokenBalances, useTokenBalance } from 'state/wallet/hooks';
+import styled, { ThemeContext } from 'styled-components';
+import { CloseIcon, LinkStyledButton } from 'theme';
 import Column from '../../components/Column';
 import Modal from '../../components/Modal';
-import { filterTokens } from '../../components/SearchModal/filtering';
 import { PaddedColumn, SearchInput } from '../../components/SearchModal/styleds';
 import Tooltip from '../../components/Tooltip';
 import { isAddress } from '../../utils';
-import List from './TokensList';
+import CommonBases from 'components/SearchModal/CommonBases';
+import CurrencyList from 'components/SearchModal/CurrencyList';
+import Loader from 'components/Loader';
+
+const LoaderBox = styled.div`
+  width: 100%;
+  height: 500px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default function SearchModal({
-  tokens = [],
-  handleSelect = (some: any) => {},
-  isLoading = false,
-  isOpen = false,
+  tokens,
+  isOpen,
   onDismiss,
-  onSelect,
+  onCurrencySelect,
+  hiddenCurrency,
+  showSendWithSwap,
+  otherSelectedCurrency,
+  showCommonBases = false,
+  isMatchEth = false,
+  isLpTokens = false,
 }) {
   const { t } = useTranslation();
-  const inputRef = useRef<HTMLInputElement>();
+  const { account, chainId } = useActiveWeb3React();
+  const theme = useContext(ThemeContext);
 
-  const [tooltipOpen, setTooltipOpen] = useState<boolean>(false);
   const [searchQuery, setSearchQuery] = useState<string>('');
+  const [tooltipOpen, setTooltipOpen] = useState<boolean>(false);
+  const [invertSearchOrder, setInvertSearchOrder] = useState<boolean>(false);
+  const [allTokens, isLoading] = useAllTokens(isLpTokens);
 
-  const availableToken = tokens.filter(t => t.symbol);
+  // if the current input is an address, and we don't have the token in context, try to fetch it and import
+  const searchToken = useToken(searchQuery);
+  const searchTokenBalance = useTokenBalance(account, searchToken);
+  const allTokenBalances = useAllTokenBalances();
 
-  const filteredTokens = useMemo(() => {
-    return filterTokens(Object.values(availableToken), searchQuery);
-  }, [availableToken, searchQuery]);
+  const getVisibleTokenBalances = (): { [token: string]: TokenAmount } => {
+    let balances = allTokenBalances || {};
+    if (searchToken) {
+      balances = {
+        [searchToken.address]: searchTokenBalance,
+      };
+    }
+    return balances;
+  };
 
-  const filteredSortedTokens = useMemo(() => {
-    const sorted = filteredTokens.sort((a, b) =>
-      a.symbol.localeCompare(b.symbol, 'es', { sensitivity: 'base' }),
-    );
+  const visibleTokenBalances = getVisibleTokenBalances();
+
+  const tokenComparator = useTokenComparator(invertSearchOrder);
+
+  const filteredTokens: Token[] = useMemo(() => {
+    if (searchToken) {
+      return [searchToken];
+    }
+    return filterTokens(Object.values(allTokens), searchQuery, isLpTokens);
+  }, [searchToken, allTokens, searchQuery, isLpTokens]);
+
+  const filteredSortedTokens: Token[] = useMemo(() => {
+    if (searchToken) {
+      return [searchToken];
+    }
+    const sorted = filteredTokens.sort(tokenComparator);
     const symbolMatch = searchQuery
       .toLowerCase()
       .split(/\s+/)
       .filter(s => s.length > 0);
-    if (symbolMatch.length > 1) return sorted;
+    if (symbolMatch.length > 1) {
+      return sorted;
+    }
 
     return [
+      ...(searchToken ? [searchToken] : []),
       // sort any exact symbol matches first
       ...sorted.filter(token => token.symbol.toLowerCase() === symbolMatch[0]),
       ...sorted.filter(token => token.symbol.toLowerCase() !== symbolMatch[0]),
     ];
-  }, [filteredTokens, searchQuery]);
+  }, [filteredTokens, searchQuery, searchToken, tokenComparator]);
 
-  // const openTooltip = useCallback(() => {
-  //   setTooltipOpen(true);
-  // }, [setTooltipOpen]);
+  const handleCurrencySelect = useCallback(
+    (currency: Token) => {
+      onCurrencySelect(currency);
+      onDismiss();
+    },
+    [onDismiss, onCurrencySelect],
+  );
+
+  // clear the input on open
+  useEffect(() => {
+    if (isOpen) {
+      setSearchQuery('');
+    }
+  }, [isOpen, setSearchQuery]);
+
+  // manage focus on modal show
+  const inputRef = useRef<HTMLInputElement>();
+  const handleInput = useCallback(event => {
+    const input = event.target.value;
+    const checksummedInput = isAddress(input);
+    setSearchQuery(checksummedInput || input);
+    setTooltipOpen(false);
+  }, []);
+
+  const openTooltip = useCallback(() => {
+    setTooltipOpen(true);
+  }, [setTooltipOpen]);
   const closeTooltip = useCallback(() => setTooltipOpen(false), [setTooltipOpen]);
+
+  useInterval(
+    () => {
+      setTooltipOpen(false);
+    },
+    tooltipOpen ? 4000 : null,
+    false,
+  );
 
   const handleEnter = useCallback(
     (e: KeyboardEvent<HTMLInputElement>) => {
@@ -58,24 +152,12 @@ export default function SearchModal({
           filteredSortedTokens[0].symbol.toLowerCase() === searchQuery.trim().toLowerCase() ||
           filteredSortedTokens.length === 1
         ) {
-          handleSelect(filteredSortedTokens[0]);
+          handleCurrencySelect(filteredSortedTokens[0]);
         }
       }
     },
-    [filteredSortedTokens, handleSelect, searchQuery],
+    [filteredSortedTokens, handleCurrencySelect, searchQuery],
   );
-
-  const handleInput = useCallback(event => {
-    const input = event.target.value;
-    const checksummedInput = isAddress(input);
-    setSearchQuery(checksummedInput || input);
-    setTooltipOpen(false);
-  }, []);
-
-  // clear the input on open
-  useEffect(() => {
-    if (isOpen) setSearchQuery('');
-  }, [isOpen, setSearchQuery]);
 
   return (
     <Modal
@@ -87,6 +169,16 @@ export default function SearchModal({
     >
       <Column style={{ width: '100%' }}>
         <PaddedColumn gap="16px">
+          <RowBetween>
+            <Text fontWeight={500} fontSize={16}>
+              Select a token
+              <QuestionHelper
+                disabled={tooltipOpen}
+                text="Find a token by searching for its name or symbol or by pasting its address below."
+              />
+            </Text>
+            <CloseIcon onClick={onDismiss} />
+          </RowBetween>
           <Tooltip
             text="Import any token into your list by pasting the token address into the search field."
             show={tooltipOpen}
@@ -104,8 +196,54 @@ export default function SearchModal({
               onKeyDown={handleEnter}
             />
           </Tooltip>
+          {showCommonBases && (
+            <CommonBases
+              chainId={chainId}
+              onSelect={handleCurrencySelect}
+              selectedCurrency={hiddenCurrency}
+              otherSelectedCurrency={otherSelectedCurrency}
+            />
+          )}
+          <RowBetween>
+            <Text fontSize={14} fontWeight={500}>
+              Token Name
+            </Text>
+            <SortButton
+              ascending={invertSearchOrder}
+              toggleSortOrder={() => setInvertSearchOrder(iso => !iso)}
+            />
+          </RowBetween>
         </PaddedColumn>
-        {isLoading ? 'LOADING' : <List items={filteredSortedTokens} onSelect={onSelect} />}
+        {isLoading ? (
+          <LoaderBox>
+            <Loader size="100px" />
+          </LoaderBox>
+        ) : (
+          <CurrencyList
+            currencies={filteredSortedTokens}
+            allBalances={visibleTokenBalances}
+            onCurrencySelect={handleCurrencySelect}
+            otherCurrency={otherSelectedCurrency}
+            selectedCurrency={hiddenCurrency}
+            showSendWithSwap={showSendWithSwap}
+            showName={isLpTokens}
+            isMatchEth={isMatchEth}
+            isLpTokens={isLpTokens}
+          />
+        )}
+        <div style={{ height: '1px', backgroundColor: theme.bg2, margin: '0 30px' }} />
+        <Card>
+          <AutoRow justify={'center'}>
+            <div>
+              <LinkStyledButton
+                style={{ fontWeight: 500, color: theme.text2, fontSize: 16 }}
+                onClick={openTooltip}
+              >
+                Having trouble finding a token?
+              </LinkStyledButton>
+            </div>
+          </AutoRow>
+        </Card>
       </Column>
     </Modal>
   );
