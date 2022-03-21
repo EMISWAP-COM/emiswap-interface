4b83fde3a97aff6a9a039c531d1e23f293d3ca66
DENIS LUKIN <nash1q@yandex.ru>
2022-03-21 14:47:09 +0300

    bridge refactoring

--- src/pages/Bridge/api.ts
+++ src/pages/Bridge/api.ts
@@ -28,6 +28,74 @@ export const apiSlice = createApi({
       transformResponse: (response: { result: { token: Token; chainId: number }[] }) =>
         getUniqueTokens(response.result),
     }),
+    getBuildTx: builder.query<any, { chainID; owner; allowanceTarget; tokenAddress }>({
+      query: ({ chainID, owner, allowanceTarget, tokenAddress }) =>
+        `v1/approval/check-allowance?${new URLSearchParams({
+          chainID,
+          owner,
+          allowanceTarget,
+          tokenAddress,
+        }).toString()}`,
+      transformResponse: (response: { result: { token: Token; chainId: number }[] }) =>
+        getUniqueTokens(response.result),
+    }),
+    getApprovalBuildTx: builder.query<
+      any,
+      { chainId; owner; allowanceTarget; tokenAdress; amount }
+    >({
+      query: ({ chainId, owner, allowanceTarget, tokenAdress, amount }) =>
+        `v1/approval/build-tx?${new URLSearchParams({
+          chainId,
+          owner,
+          allowanceTarget,
+          tokenAdress,
+          amount,
+        }).toString()}`,
+      transformResponse: (response: { result: { token: Token; chainId: number }[] }) =>
+        getUniqueTokens(response.result),
+    }),
+    getCheckAllowance: builder.query<
+      any,
+      {
+        recipient;
+        fromAsset;
+        fromChainId;
+        toAsset;
+        toChainId;
+        amount;
+        output;
+        fromAddress;
+        routePath;
+      }
+    >({
+      query: ({
+        recipient,
+        fromAsset,
+        fromChainId,
+        toAsset,
+        toChainId,
+        amount,
+        output,
+        fromAddress,
+        routePath,
+      }) =>
+        `v1/send/build-tx?${new URLSearchParams({
+          recipient,
+          fromAsset,
+          fromChainId,
+          toAsset,
+          toChainId,
+          amount,
+          output,
+          fromAddress,
+          routePath,
+        }).toString()}`,
+      transformResponse: (response: { result: { token: Token; chainId: number }[] }) =>
+        getUniqueTokens(response.result),
+    }),
+    getRoutes: builder.query<{ result: Chain[] }, void>({
+      query: () => 'v1/supported/bridges',
+    }),
     getQuote: builder.query<
       { result?: Quote },
       {
@@ -57,5 +125,9 @@ export const {
   useGetSupportTokenFromQuery,
   useGetSupportTokenToQuery,
   useGetQuoteQuery,
+  useGetRoutesQuery,
+  useGetCheckAllowanceQuery,
+  useGetApprovalBuildTxQuery,
+  useGetBuildTxQuery,
 } = apiSlice;
 export default apiSlice;
