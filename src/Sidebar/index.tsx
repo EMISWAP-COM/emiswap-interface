a2931a705348c318254b7fc251f0807552183d76
Alexey Semikozov <alwexy@gmail.com>
2022-03-24 15:36:24 +0300

    Add styled design types

--- src/Sidebar/index.tsx
+++ src/Sidebar/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
-import { color, layout, width } from 'styled-system';
+import { color, layout, width, ColorProps, LayoutProps, WidthProps } from 'styled-system';
 
-const SidebarWrapper = styled.div`
+interface Props extends ColorProps, LayoutProps, WidthProps {
+  children: React.ReactNode;
+}
+
+const SidebarWrapper = styled.div<Props>`
   ${color}
   ${layout}
   ${width}
 `;
 
-const Sidebar = () => <SidebarWrapper>Sidebar</SidebarWrapper>;
+const Sidebar = () => <SidebarWrapper width={1 / 2}>Sidebar</SidebarWrapper>;
 
 export default Sidebar;
