60520daf047bd4ea38aed2be0d9e6f2fce132da5
DENIS LUKIN <nash1q@yandex.ru>
2022-03-29 19:10:52 +0300

    Rewrite css logic [ESRD-18]

--- src/Landing/MainBlock.tsx
+++ src/Landing/MainBlock.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Flex, Grid, Head, Text, Image } from 'ThemeProvider';
-import mainLogo from '../assets/landing/main-block.png';
+import mainLogo from 'assets/landing/main-block.png';
 
 const MainBlock = () => (
-  <Grid height={512} gridTemplateColumns="660px auto" gridColumnGap={40} bg="bg">
-    <Flex flexDirection="column" justifyContent="flex-end" pl={60}>
+  <Grid height={480} width={1084} alignItems="center" gridTemplateColumns="600px auto">
+    <Flex flexDirection="column" justifyContent="center">
       <Head variant="largeSora" color="white">
         Next-Gen DeFi Matrix with 365% Cross-Chain APR & DAO Governance
       </Head>
@@ -12,7 +12,7 @@ const MainBlock = () => (
         Extract the highest daily yields from liquidity mining and farming
       </Text>
     </Flex>
-    <Flex flexDirection="column" justifyContent="flex-end">
+    <Flex>
       <Image backgroundImage={`url(${mainLogo})`} backgroundSize="cover" width={470} height={480} />
     </Flex>
   </Grid>
