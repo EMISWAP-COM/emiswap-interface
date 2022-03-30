06a0aa62690baeb3b8a60adae7152cdad4e02888
Denis Lukin <frontenddeveloper.outstaf1@emidao.org>
2022-03-30 12:21:56 +0000

    Refactor design-system component logic [ESRD-18]

--- src/Landing/MainBlock.tsx
+++ src/Landing/MainBlock.tsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { Flex, Grid, Head, Text, Image } from 'ThemeProvider';
+import mainLogo from 'assets/landing/main-block.png';
 
-const MainBlock = () => <span>Main Block</span>;
+const MainBlock = () => (
+  <Grid height="30rem" width="67.75rem" gridColumnGap="3.75rem" gridTemplateColumns="37.5rem auto">
+    <Flex flexDirection="column" justifyContent="center">
+      <Head variant="largeSora" color="white">
+        Next-Gen DeFi Matrix with 365% Cross-Chain APR & DAO Governance
+      </Head>
+      <Text color="white" variant="largeRubik" width="30.94rem">
+        Extract the highest daily yields from liquidity mining and farming
+      </Text>
+    </Flex>
+    <Flex>
+      <Image
+        backgroundImage={`url(${mainLogo})`}
+        backgroundSize="cover"
+        width="29.38rem"
+        height="30rem"
+      />
+    </Flex>
+  </Grid>
+);
 
 export default MainBlock;
