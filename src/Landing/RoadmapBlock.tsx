1f33b4b313eba593525cb36fa6eacd82bc0c65a7
Daniil Orain <dnlorn@gmail.com>
2022-03-31 02:16:26 +0400

    add QUARTALS_INFO obj

--- src/Landing/RoadmapBlock.tsx
+++ src/Landing/RoadmapBlock.tsx
@@ -1,13 +1,142 @@
 import React from 'react';
 import { Flex, Image, StyledHead, Text } from '../ThemeProvider';
 import GreenToPurpleRectangle from '../assets/svg/Rectangles/GreenToPurple.svg';
+import PurpleToGreenRectangle from '../assets/svg/Rectangles/PurpleToGreen.svg';
 import RoadmapLineXL from '../assets/svg/Lines/RoadmapLineXL.svg';
 import styled from 'styled-components';
 
-const QuartalBlock = styled(Flex)`
-`;
+interface IStep {
+  name: string;
+  icon: string;
+  description?: string;
+}
 
+interface IQuartal {
+  name: string;
+  progressIcon: string;
+  roadmapStepsIcon: string;
+  steps: Array<IStep>;
+  isActive?: Boolean;
+}
 
+interface IQuartalsInfo {
+  [index: string]: IQuartal;
+}
+
+const Q1 = {
+  name: 'Q1',
+  progressIcon: GreenToPurpleRectangle,
+  roadmapStepsIcon: '#',
+  isActive: true,
+  steps: [
+    {
+      name: 'Shiden',
+      icon: '#',
+    },
+    {
+      name: 'Near/Aurora',
+      icon: '#',
+    },
+    {
+      name: 'Avalanche',
+      icon: '#',
+    },
+    {
+      name: 'Gate Chain',
+      icon: '#',
+    },
+    {
+      name: 'Cross-chain bridge',
+      description: 'Stage 01',
+      icon: '#',
+    },
+    {
+      name: 'Cross-chain bridge',
+      description: 'Stage 02',
+      icon: '#',
+    }
+  ],
+};
+const Q2 = {
+  name: 'Q2',
+  progressIcon: PurpleToGreenRectangle,
+  roadmapStepsIcon: '#',
+  steps: [
+    {
+      name: 'Binance Smart Chain',
+      icon: '#',
+    },
+    {
+      name: 'Solana',
+      icon: '#',
+    },
+    {
+      name: 'Heco',
+      icon: '#',
+    },
+    {
+      name: 'Platform Redesign',
+      icon: '#',
+    }
+  ]
+};
+const Q3 = {
+  name: 'Q3',
+  progressIcon: GreenToPurpleRectangle,
+  roadmapStepsIcon: '#',
+  steps: [
+    {
+      name: 'OKEX',
+      icon: '#',
+    },
+    {
+      name: 'Tron',
+      icon: '#',
+    },
+    {
+      name: 'NFT magic hall',
+      description: 'Stage 01',
+      icon: '#',
+    }
+  ]
+};
+const Q4 = {
+  name: 'Q4',
+  progressIcon: PurpleToGreenRectangle,
+  roadmapStepsIcon: '#',
+  steps: [
+    {
+      name: 'To be announced',
+      icon: '#',
+    },
+    {
+      name: 'NFT magic hall',
+      description: 'Stage 02',
+      icon: '#',
+    }
+  ]
+};
+
+const QUARTALS_INFO: IQuartalsInfo = {
+  'q1': Q1,
+  'q2': Q2,
+  'q3': Q3,
+  'q4': Q4,
+};
+
+const QuartalBlock = ({ quartal } : {quartal: IQuartal}) => (
+  <div>
+    <Flex alignItems="center">
+      <Text>{quartal.name}</Text>
+      <Image
+        backgroundImage={`url(${quartal.progressIcon})`}
+        backgroundSize="cover"
+        width="12.1875rem"
+        height="0.3125rem"
+      />
+    </Flex>
+  </div>
+);
 
 const RoadmapBlock = () => (
   <Flex flexDirection="column">
@@ -21,61 +150,9 @@ const RoadmapBlock = () => (
       </Text>
     </header>
     <Flex marginTop="2.5rem">
-      <QuartalBlock>
-        <Flex alignItems="center">
-          <Text color="text">Q1</Text>
-          <Image
-            marginLeft="0.75rem"
-            backgroundImage={`url(${GreenToPurpleRectangle})`}
-            backgroundSize="cover"
-            width="12.1875rem"
-            height="0.3125rem"
-          />
-        </Flex>
-        <Flex>
-          <Image
-            backgroundImage={`url(${RoadmapLineXL}`}
-            backgroundSize="cover"
-            width="0.5625rem"
-            height="10rem"
-          />
-        </Flex>
-      </QuartalBlock>
-      <QuartalBlock marginLeft="0.75rem">
-        <Flex alignItems="center">
-          <Text>Q2</Text>
-          <Image
-            backgroundImage={`url(${GreenToPurpleRectangle})`}
-            backgroundSize="cover"
-            width="12.1875rem"
-            height="0.3125rem"
-          />
-        </Flex>
-      </QuartalBlock>
-      <QuartalBlock marginLeft="0.75rem">
-        <Flex alignItems="center">
-          <Text>Q3</Text>
-          <Image
-            backgroundImage={`url(${GreenToPurpleRectangle})`}
-            backgroundSize="cover"
-            width="12.1875rem"
-            height="0.3125rem"
-          />
-        </Flex>
-        <Flex>
-        </Flex>
-      </QuartalBlock>
-      <QuartalBlock marginLeft="0.75rem">
-        <Flex alignItems="center">
-          <Text>Q4</Text>
-          <Image
-            backgroundImage={`url(${GreenToPurpleRectangle})`}
-            backgroundSize="cover"
-            width="12.1875rem"
-            height="0.3125rem"
-          />
-        </Flex>
-      </QuartalBlock>
+      {Object.keys(QUARTALS_INFO).map((QUARTAL_INFO_KEY) => (
+        <QuartalBlock quartal={QUARTALS_INFO[QUARTAL_INFO_KEY]} />
+      ))}
     </Flex>
   </Flex>
 );
