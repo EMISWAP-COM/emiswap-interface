f7b06e9bb2d9c42d081eeafc73415a3ff2951942
DENIS LUKIN <nash1q@yandex.ru>
2022-03-31 14:55:09 +0300

    Refactor Partners Block

--- src/Landing/PartnersBlock.tsx
+++ src/Landing/PartnersBlock.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Grid, Flex, StyledHead, SVG } from 'ThemeProvider';
-import maskGroupLogo from 'assets/svg/mask-group-logo.svg';
-import digfinexLogo from 'assets/svg/digfinex-logo.svg';
-import everestLogo from 'assets/svg/everest-logo.svg';
-import emirexLogo from 'assets/svg/emirex-logo.svg';
-import bigbrainLogo from 'assets/svg/bigbrain-logo.svg';
-import alphaLogo from 'assets/svg/alpha-logo.svg';
-import etexchangeLogo from 'assets/svg/etexchange-logo.svg';
-import mahdaoLogo from 'assets/svg/mahdao-logo.svg';
-import jpycLogo from 'assets/svg/jpyc-logo.svg';
-import holaLogo from 'assets/svg/hola-logo.svg';
-import transformLogo from 'assets/svg/transform-group-logo.svg';
+import { Grid, Flex, StyledHead, Image } from 'ThemeProvider';
+import bitmartLogo from 'assets/landing/bitmart-logo.png';
+import digfinexLogo from 'assets/landing/digfinex-logo.png';
+import everestLogo from 'assets/landing/everest-logo.png';
+import emirexLogo from 'assets/landing/emirex-logo.png';
+import bigbrainLogo from 'assets/landing/bigbrain-logo.png';
+import alphaLogo from 'assets/landing/alpha-logo.png';
+import etexchangeLogo from 'assets/landing/etexchange-logo.png';
+import mahdaoLogo from 'assets/landing/mahdao-logo.png';
+import jpycLogo from 'assets/landing/jpyc-logo.png';
+import holaLogo from 'assets/landing/holla-logo.png';
+import transformLogo from 'assets/landing/transform-group-logo.png';
 
 const PartnersBlock = () => (
   <Grid
@@ -26,22 +26,77 @@ const PartnersBlock = () => (
       <StyledHead>Partners</StyledHead>
     </Flex>
     <Flex flexDirection="column">
-      <Flex justifyContent="space-between" mt="1rem" alignItems="center">
-        <SVG backgroundImage={`url(${maskGroupLogo})`} width="8.95rem" height="4rem" />
-        <SVG backgroundImage={`url(${digfinexLogo})`} width="8.95rem" height="3.5rem" />
-        <SVG backgroundImage={`url(${everestLogo})`} width="8.95rem" height="3.5rem" />
-        <SVG backgroundImage={`url(${emirexLogo})`} width="8.95rem" height="3.8rem" />
+      <Flex justifyContent="space-between" mt="2rem" alignItems="center">
+        <Image
+          backgroundImage={`url(${bitmartLogo})`}
+          backgroundSize="cover"
+          width="7.809rem"
+          height="2.146rem"
+        />
+        <Image
+          backgroundImage={`url(${digfinexLogo})`}
+          backgroundSize="cover"
+          width="8.438rem"
+          height="1.625rem"
+        />
+        <Image
+          backgroundImage={`url(${everestLogo})`}
+          backgroundSize="cover"
+          width="8.1885rem"
+          height="1.625rem"
+        />
+        <Image
+          backgroundImage={`url(${emirexLogo})`}
+          backgroundSize="cover"
+          width="7.75rem"
+          height="2.15rem"
+        />
       </Flex>
-      <Flex justifyContent="space-between" pl="5.5rem" pr="5.5rem" alignItems="center" mt="1rem">
-        <SVG backgroundImage={`url(${bigbrainLogo})`} width="8.95rem" height="3.5rem" />
-        <SVG backgroundImage={`url(${alphaLogo})`} width="12.8rem" height="3.8rem" />
-        <SVG backgroundImage={`url(${etexchangeLogo})`} width="8.95rem" height="2.5rem" />
+      <Flex justifyContent="space-between" pl="5.5rem" pr="5.5rem" alignItems="center" mt="2rem">
+        <Image
+          backgroundImage={`url(${bigbrainLogo})`}
+          backgroundSize="cover"
+          width="8.063rem"
+          height="2.219rem"
+        />
+        <Image
+          backgroundImage={`url(${alphaLogo})`}
+          backgroundSize="cover"
+          width="12.694rem"
+          height="2.139rem"
+        />
+        <Image
+          backgroundImage={`url(${etexchangeLogo})`}
+          backgroundSize="cover"
+          width="10.938rem"
+          height="1rem"
+        />
       </Flex>
-      <Flex justifyContent="space-between" alignItems="center" mt="1rem">
-        <SVG backgroundImage={`url(${mahdaoLogo})`} width="8.95rem" height="3.2rem" />
-        <SVG backgroundImage={`url(${jpycLogo})`} width="8.95rem" height="3.5rem" />
-        <SVG backgroundImage={`url(${holaLogo})`} width="8.95rem" height="3.5rem" />
-        <SVG backgroundImage={`url(${transformLogo})`} width="8.95rem" height="4.6rem" />
+      <Flex justifyContent="space-between" alignItems="center" mt="2rem">
+        <Image
+          backgroundImage={`url(${mahdaoLogo})`}
+          backgroundSize="cover"
+          width="9.875rem"
+          height="2.125rem"
+        />
+        <Image
+          backgroundImage={`url(${jpycLogo})`}
+          backgroundSize="cover"
+          width="6.495rem"
+          height="2.146rem"
+        />
+        <Image
+          backgroundImage={`url(${holaLogo})`}
+          backgroundSize="cover"
+          width="6.813rem"
+          height="2.125rem"
+        />
+        <Image
+          backgroundImage={`url(${transformLogo})`}
+          backgroundSize="cover"
+          width="8.25rem"
+          height="2.875rem"
+        />
       </Flex>
     </Flex>
   </Grid>
