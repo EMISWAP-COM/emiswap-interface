37125933e48d21d81dd0b48018c8bdf53e21be37
DENIS LUKIN <nash1q@yandex.ru>
2022-04-06 17:01:43 +0300

    fix infoblock

--- src/Landing/InfoBlock.tsx
+++ src/Landing/InfoBlock.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Flex, Grid, Head, Text, Image, StyledHead } from 'ThemeProvider';
+import { Flex, Grid, Image, StyledHead } from 'ThemeProvider';
+import Text from 'ui-kit/Text';
 import infoBlock from 'assets/landing/info-block.png';
 
 const InfoBlock = () => (
   <Grid
     height="33.8rem"
-    width="67.75rem"
-    gridColumnGap="3.75rem"
-    gridTemplateColumns="35.2rem auto"
-    mt="7.5rem"
+    width="65rem"
+    gridColumnGap={6}
+    gridTemplateColumns="37rem auto"
+    mt={7}
     ml="auto"
     mr="auto"
   >
@@ -22,7 +23,7 @@ const InfoBlock = () => (
     </Flex>
     <Flex flexDirection="column" justifyContent="center">
       <StyledHead>Meet the EmiDAO Ecosystem</StyledHead>
-      <Text variant="largeRubik" color="white" mt="1.86rem">
+      <Text textStyle="largeRubikRegular" color="white">
         EmiSwap is an AMM DEX and the first project in the EmiDAO ecosystem. 100% of the trading
         fees on the DEX are distributed between liquidity providers and ESW token holders.
       </Text>
