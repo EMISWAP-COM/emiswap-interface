68d653826b9697bffb90bdadddb06fe8a7bf9903
Daniil Orain <frontend.dev1@emidao.org>
2022-04-06 08:16:12 +0000

    Feature/ESRD-61

--- src/ThemeProvider/theme.tsx
+++ src/ThemeProvider/theme.tsx
@@ -27,6 +27,7 @@ const theme = {
     buttonInactive: '#7A2DF426',
     drop: '#1B1B1F',
     disabled: 'rgba(255, 255, 255, 0.7)',
+    almostNoWhite: 'rgba(255, 255, 255, 0.05)'
   },
   gradients: {
     buttonPrimary:
---
+++ src/ui-kit/table/header.tsx
@@ -0,0 +1,35 @@
+import React, { ReactElement } from 'react';
+import styled from 'styled-components';
+import { color, typography, TypographyProps } from 'styled-system';
+import { Flex, Text } from 'ThemeProvider';
+import { CellWrapper } from './cell';
+
+interface HeaderInterface {
+  columns?: Array<string>;
+}
+
+const TableHeader = styled(Flex)<TypographyProps>`
+  ${typography};
+  ${color};
+`;
+
+const Header = ({ columns }: HeaderInterface): ReactElement => {
+  return (
+    <CellWrapper px={1} mb={1}>
+      {columns.map((column, index) => {
+        const isLeftest = index === 0;
+        const isRightest = index === columns.length - 1;
+        const textAlign = isLeftest ? 'left' : isRightest ? 'right' : 'center';
+        return (
+          <TableHeader textAlign={textAlign}>
+            <Text variant="smallRubik" color="disabled">
+              {column}
+            </Text>
+          </TableHeader>
+        );
+      })}
+    </CellWrapper>
+  );
+};
+
+export default Header;
