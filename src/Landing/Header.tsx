eb3d33b1c84d3c873ad4336fbea4e11734527efd
DENIS LUKIN <nash1q@yandex.ru>
2022-04-11 10:51:39 +0300

    Added base logic

--- src/Landing/Header.tsx
+++ src/Landing/Header.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Select from 'ui-kit/select';
+import { ESWIcon } from 'ui-kit/icons';
 
-const Header = () => <span>Header</span>;
+const Header = () => <Select icon={ESWIcon} value="Change Network" />;
 
 export default Header;
