1313c2b0cdf4f68c9d92e1e3118141f80dbf6de2
Nikita Bezhin <bezhin.nikita@gmail.com>
2022-05-05 20:35:49 +0300

    Fix APR

--- src/pages/Farm365/helpers.ts
+++ src/pages/Farm365/helpers.ts
@@ -8,19 +8,19 @@ export function calcFarming365Apr(
   eswRate: number,
 ): number {
   let dayBlocksCount = 36000;
-  let perSeconds = 1;
 
   if (chainId === chainIds.SHIDEN) {
     dayBlocksCount = 7200;
   } else if (chainId === chainIds.ASTAR) {
     dayBlocksCount = 7200;
-    perSeconds = 12;
   }
 
   const liq = parseFloat(liquidity);
   const reward = parseFloat(blockReward);
 
-  return 365 + (dayBlocksCount * 365 * reward * eswRate * 100 * perSeconds) / (liq + 1);
+  console.log(365, dayBlocksCount, 365, reward, eswRate, 100, liq, 1);
+
+  return 365 + (dayBlocksCount * 365 * reward * eswRate * 100) / (liq + 1);
 }
 
 export function calcAprValue(token: Token): number {
