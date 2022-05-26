fd15b1d30c64a0e57e8641e67dd8a12ccfcc7880
Daniil Orain <dnlorn@gmail.com>
2022-05-26 14:31:08 +0300

    remove console

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -20,7 +20,6 @@ export function useActiveWeb3React(): Web3ReactContextInterface<Web3Provider> & 
 } {
   const context = useWeb3ReactCore<Web3Provider>();
   const contextNetwork = useWeb3ReactCore<Web3Provider>(NetworkContextName);
-  console.log('context: ', context, 'Web3Provider: ', Web3Provider);
   return context.active ? context : contextNetwork;
 }
 
