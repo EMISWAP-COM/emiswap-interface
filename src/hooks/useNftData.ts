7fba03d34f0cadf3d4dc9a1075e92eead7392b8d
Nikita Bezhin <bezhin.nikita@gmail.com>
2022-06-22 12:57:46 +0300

    ES-648: NFT loading

--- src/hooks/useNftData.ts
+++ src/hooks/useNftData.ts
@@ -2,18 +2,43 @@
 import nftPurplePng from '../assets/images/nft-purple.png';
 // @ts-ignore
 import nftGreenPng from '../assets/images/nft-green.png';
+import { useActiveWeb3React } from './index';
+import { useEffect, useState } from 'react';
+import { fetchWrapper } from '../api/fetchWrapper';
 
 export default function useNftData() {
-  const nfts = [
-    {
-      name: 'Basic level NFT',
-      img: nftPurplePng,
-    },
-    {
-      name: 'Top level NFT',
-      img: nftGreenPng,
-    },
-  ];
+  const { /*library, chainId,*/ account } = useActiveWeb3React();
+
+  // const contract = getPolygonNftContract(library!, '0x527b0726d7817ce8a3281a137276e6950efb0a3c', chainId!);
+
+  const [nfts, setNfts] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchWrapper
+      .get(
+        `https://api.polygonscan.com/api?` +
+          new URLSearchParams({
+            module: 'account',
+            action: 'tokennfttx',
+            apikey: '58MJVC5HYTPBNYZDJIXBUD63RKACESMMU5',
+            address: account!, // '0xC1f77e2D09bbB37135D069e969854582B0EaB975',
+          }),
+      )
+      .then(response => {
+        const items = response?.result?.map((item: any, index: number) => {
+          return {
+            name: index === 0 ? 'Basic level NFT' : 'Top level NFT',
+            img: index === 0 ? nftPurplePng : nftGreenPng,
+          };
+        });
+        setNfts(items);
+      });
+
+    /* contract.balanceOf('0xC1f77e2D09bbB37135D069e969854582B0EaB975')
+       .then((value) => {
+         console.log(value);
+       });*/
+  }, [account]);
 
   return {
     nfts,
