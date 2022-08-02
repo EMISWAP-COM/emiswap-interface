7e791ff6b126b2ef007bdb890d1b0dcdc97d37ec
Nikita Bezhin <bezhin.nikita@gmail.com>
2022-08-02 15:48:36 +0300

    Fix texts

--- src/components/AccountDetails/Common/ESWRewards.tsx
+++ src/components/AccountDetails/Common/ESWRewards.tsx
@@ -131,11 +131,11 @@ export const ESWRewards = () => {
       {(network === 'polygon' || network === 'mumbai') && (
         <RewardsWrapperPolygon>
           <Item
-            text="180% APR campaign"
+            text="180% LP campaign"
             count={convertBigDecimal((grouped as any).bonus_180?.ESW ?? '0')}
           />
           <Item
-            text="365+% APR campaign"
+            text="365+% Farm campaign"
             count={convertBigDecimal((grouped as any).bonus_365?.ESW ?? '0')}
           />
           <Item text="Referral Reward" count={convertBigDecimal(referalRevards.toString())} />
